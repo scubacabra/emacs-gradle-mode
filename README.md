5eee409914cedc2c0aed070970ec790fddd595c7
Daniel Mijares <daniel.j.mijares@gmail.com>
2013-06-14 12:51:28 -0400

    finding the executable, and concatting a string together with variable to pass to the command line with the compile function.  Starting to come together

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 emacs-gradle-mode
 =================
 
-minor mode for emacs to run gradle from emacs and not have to go to a terminal
+minor mode for emacs to run gradle from emacs and not have to go to a terminal!
+
+# To Install #
+
+```lisp
+(require 'gradle-mode)
+```
+
+# Add this to a mode hook #
+```lisp
+(add-hook 'groovy-mode-hook 'gradle-mode)
+```
+
+etc. etc. for `java-mode` or `scala-mode` or whatever. 
--- gradle-mode.el
+++ gradle-mode.el
@@ -1,4 +1,4 @@
-;;; gradle-mode.el --- Groovy and gradle-mode Emacs features
+;;; gradle-mode.el --- gradle integration with Emacs
 
 ;; This is free and unencumbered software released into the public domain.
 
@@ -11,18 +11,41 @@
 
 ;;; Code:
 
-(defvar gradle-mode-map (make-sparse-keymap)
+(defcustom gradle-executable "gradle"
+  "If gradle is not in `exec-path', this should contain the full
+path to gradle executable."
+  :type 'string
+  :group 'gradle)
+
+(defun gradle--executable-path ()
+  (executable-find gradle-executable))
+
+(defun gradle-run (tasks)
+  ;; (interactive (list (gradle--input-commandline)))
+  (interactive)
+  ;; (gradle--with-project-root
+   (print (concat (gradle--executable-path)
+                    (when tasks
+                      (mapconcat 'identity (cons "" tasks) " ")))))
+
+(defvar gradle-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "C-c C-g c") (gradle-run (list "compile")))
+    (define-key map (kbd "C-c C-g b") (gradle-run (list "build")))
+    (define-key map (kbd "C-c C-g t") (gradle-run (list "test")))
+    (define-key map (kbd "C-c C-g C-d c") (gradle-run (list "compile" "--daemon")))
+    (define-key map (kbd "C-c C-g C-d b") (gradle-run (list "build"   "--daemon")))
+    (define-key map (kbd "C-c C-g C-d t") (gradle-run (list "test"    "--daemon")))
+    (define-key map (kbd "C-c C-g d") (print "Run something as daemon"))
+    (define-key map (kbd "C-c C-g r") (print "Run something as normal"))
+    map)
   "Keymap for the gradle minor mode.")
 
 ;;;###autoload
 (define-minor-mode gradle-mode
-  "Extensions to groovy-mode for further support with standard Groovy tools."
+  "Gradle Mode -- run gradle from any buffer, will scan up for nearest gradle directory and run the command."
   :lighter " gra"
   :keymap 'gradle-mode-map)
-
-;; Enable the minor mode wherever groovy-mode is used.
-;;;###autoload
-(add-hook 'groovy-mode-hook 'gradle-mode)
 
 (provide 'gradle-mode)
 
