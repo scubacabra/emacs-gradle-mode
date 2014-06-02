fefa4ce01e0f6ecd814919c39b83394307e637fb
Daniel Mijares <daniel.j.mijares@gmail.com>
2014-06-01 23:18:09 -0400

    manual inclusion of this package file might not be letting the melpa parser override with what is in the details for the header. So adding this to the file to see if that solves any problems.

--- gradle-mode-pkg.el
+++ gradle-mode-pkg.el
@@ -1,2 +1,4 @@
-(define-package "gradle-mode" "0.5"
-                "Gradle integration for Emacs' compilation mode.")
+(define-package
+  "gradle-mode" "0.5"
+  "Gradle integration for Emacs' compilation mode."
+  '((s "1.8.0") (dash "2.2.0") (f "0.16.0")))
